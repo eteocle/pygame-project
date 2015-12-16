eec7b8ba429779dee36a4e988fbbd93eb5659282
eteocle <vincent.marschall@gmail.com>
2015-12-16 20:10:51 +0100

    Beginning of a game using pygame.

---
+++ game.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.4
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+fenetre = pygame.display.set_mode((640, 480))
+
+#Variable qui continue la boucle si = 1, stoppe si = 0
+continuer = 1
+
+#Boucle infinie
+while continuer:
+    continue 
