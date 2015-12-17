d29283f29157611a5edc44f5cba308917d1146e0
eteocle <vincent.marschall@gmail.com>
2015-12-17 19:59:45 +0100

    ajout des premiers évenements aux claviers

--- game.py
+++ game.py
@@ -5,11 +5,39 @@ from pygame.locals import *
 
 pygame.init()
 
+#Ouverture de la fenêtre Pygame
 fenetre = pygame.display.set_mode((640, 480))
+
+#Chargement et collage du fond
+fond = pygame.image.load("background.jpg").convert()
+fenetre.blit(fond, (0,0))
+
+#Chargement et collage du personnage
+perso = pygame.image.load("perso.png").convert_alpha()
+position_perso = perso.get_rect()
+fenetre.blit(perso, (200,300))
+
+#Rafraîchissement de l'écran
+pygame.display.flip()
 
 #Variable qui continue la boucle si = 1, stoppe si = 0
 continuer = 1
 
+
+pygame.key.set_repeat(400, 30)
 #Boucle infinie
 while continuer:
-    continue 
+    for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
+        if event.type == QUIT:     #Si un de ces événements est de type QUIT
+            continuer = 0      #On arrête la boucle
+            continuer = 0
+        if event.type == KEYDOWN:
+            if event.key == K_DOWN: #Si "flèche bas"
+                #On descend le perso
+                position_perso = position_perso.move(0,3)
+    
+    #Re-collage
+    fenetre.blit(fond, (0,0))   
+    fenetre.blit(perso, position_perso)
+    #Rafraichissement
+    pygame.display.flip()
